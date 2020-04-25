5a256b62728424ac433e359b3f03eb53d93dc951
Victor van Wieringen <V.J.vanWieringen@student.tudelft.nl>
2020-04-25 15:40:00 +0200

    Test if breaking a test breaks the pipeline

--- app/test/bla.test.js
+++ app/test/bla.test.js
@@ -4,6 +4,6 @@ describe('testing bla.js', () => {
   test('should return num when getNum is called', () => {
     const num = 6;
     const app = new App(num);
-    expect(app.getNum()).toBe(num)
+    expect(app.getNum()).toBe(5)
   });
 });
