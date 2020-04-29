53bcb4be7442a7dafa6d054977806279bb52dab6
Ji Darwish <j.darwish@student.tudelft.nl>
2020-04-29 11:33:44 +0200

    html sliders handlers

--- app/src/scripts/parameters.js
+++ app/src/scripts/parameters.js
@@ -12,49 +12,73 @@ export function wireSlidersToHandlers(model) {
     () => {
       // TimeToSymptoms
       const timeUntilSymptomsHTML = document.getElementById('timeToSymptoms');
-      timeUntilSymptomsHTML.value = TIME_UNTIL_SYMPTOMS;
-      timeUntilSymptomsHTML.addEventListener(
-        'change',
-        (e) => (model.setTimeUntilSymptoms = e.target.value)
+      const timeUntilSymptomsOutputHTML = document.getElementById(
+        'timeToSymptomsOut'
       );
+      timeUntilSymptomsHTML.value = TIME_UNTIL_SYMPTOMS;
+      timeUntilSymptomsOutputHTML.value = `${TIME_UNTIL_SYMPTOMS} days`;
+      timeUntilSymptomsHTML.addEventListener('change', (e) => {
+        const newVal = e.target.value;
+        model.setTimeUntilSymptoms = newVal;
+        timeUntilSymptomsOutputHTML.value = `${newVal} days`;
+      });
 
       // timeUntilDetection
       const timeUntilDetectionHTML = document.getElementById(
         'timeUntilDetection'
       );
-      timeUntilDetectionHTML.value = TIME_UNTIL_DETECTION;
-      timeUntilDetectionHTML.addEventListener(
-        'change',
-        (e) => (model.setTimeUntilDetection = e.target.value)
+      const timeUntilDetectionOutputHTML = document.getElementById(
+        'timeUntilDetectionOut'
       );
+      timeUntilDetectionHTML.value = TIME_UNTIL_DETECTION;
+      timeUntilDetectionOutputHTML.value = `${TIME_UNTIL_DETECTION} days`;
+      timeUntilDetectionHTML.addEventListener('change', (e) => {
+        const newVal = e.target.value;
+        model.setTimeUntilDetection = newVal;
+        timeUntilDetectionOutputHTML.value = `${newVal} days`;
+      });
 
       // infectionCircleRadius
       const infectionCircleRadiusHTML = document.getElementById(
         'infectionCircleRadius'
       );
-      infectionCircleRadiusHTML.value = INFECTION_RADIUS;
-      infectionCircleRadiusHTML.addEventListener(
-        'change',
-        (e) => (model.setInfectionRadius = e.target.value)
+      const infectionRadiusOutoputHTML = document.getElementById(
+        'infectionRadiusOut'
       );
+      infectionCircleRadiusHTML.value = INFECTION_RADIUS;
+      infectionRadiusOutoputHTML.value = `${INFECTION_RADIUS} people`;
+
+      infectionCircleRadiusHTML.addEventListener('change', (e) => {
+        const newVal = e.target.value;
+        model.setInfectionRadius = newVal;
+        infectionRadiusOutoputHTML.value = newVal;
+      });
 
       // asymptomaticProbability
       const asymptomaticProbabilityHTML = document.getElementById(
         'asymptomaticProbability'
       );
-      asymptomaticProbabilityHTML.value = ASYMPTOMATIC_PROB;
-      asymptomaticProbabilityHTML.addEventListener(
-        'change',
-        (event) => (model.setAsymptomaticProb = event.target.value)
+      const asymptomaticProbabilityOutputHTML = document.getElementById(
+        'asymptomaticProbabilityOut'
       );
+      asymptomaticProbabilityHTML.value = ASYMPTOMATIC_PROB;
+      asymptomaticProbabilityOutputHTML.value = `${ASYMPTOMATIC_PROB * 100}%`;
+      asymptomaticProbabilityHTML.addEventListener('change', (e) => {
+        const newVal = e.target.value;
+        model.setAsymptomaticProb = newVal;
+        asymptomaticProbabilityOutputHTML.value = `${newVal * 100}%`;
+      });
 
       // agentRadius
       const agentRadiusHTML = document.getElementById('agentRadius');
+      const agentRadiusOutHTML = document.getElementById('agentRadiusOut');
       agentRadiusHTML.value = PERSON_RADIUS;
-      agentRadiusHTML.addEventListener(
-        'change',
-        (event) => (model.setPersonRadius = event.target.value)
-      );
+      agentRadiusOutHTML.value = PERSON_RADIUS;
+      agentRadiusHTML.addEventListener('change', (e) => {
+        const newVal = e.target.value;
+        model.setPersonRadius = newVal;
+        agentRadiusOutHTML.value = newVal;
+      });
 
       // number of susceptibles
       // const initSusceptibleCountHTML = document.getElementById(
