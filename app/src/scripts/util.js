dfa571a7b7034964e957c29e5fd9aa727bbfde04
Victor van Wieringen <V.J.vanWieringen@student.tudelft.nl>
2020-05-06 15:28:20 +0200

    Remove unused function

--- app/src/scripts/util.js
+++ app/src/scripts/util.js
@@ -1,15 +1,3 @@
 export function getRandom(min, max) {
   return Math.random() * (max - min) + min;
 }
-
-export function hexToRGB(hex, alpha) {
-  var r = parseInt(hex.slice(1, 3), 16),
-      g = parseInt(hex.slice(3, 5), 16),
-      b = parseInt(hex.slice(5, 7), 16);
-
-  if (alpha) {
-      return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
-  } else {
-      return "rgb(" + r + ", " + g + ", " + b + ")";
-  }
-}
