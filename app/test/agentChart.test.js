1788c7ff9e9b201772ae83902e15926aa2bdfd6a
Victor van Wieringen <V.J.vanWieringen@student.tudelft.nl>
2020-05-07 15:23:28 +0200

    That didn't work

--- app/test/agentChart.test.js
+++ app/test/agentChart.test.js
@@ -1,29 +1,26 @@
 import createContext from "gl";
 import * as agentChart from "../src/scripts/agentChart";
+var gl = require('gl')(64, 64, { preserveDrawingBuffer: true })
 
 describe('agentChart Test suite', () => {
   test('load shader successful', () => {
-      const gl = createContext(64, 64);
       const shader = agentChart.loadShader(gl, gl.VERTEX_SHADER, agentChart.vSource);
       expect(shader).not.toBe(null);
     });
 
   test('load shader unsuccessful', () => {
       const vSource = 'Random garbage'
-      const gl = createContext(64, 64);
       const shader = agentChart.loadShader(gl, gl.VERTEX_SHADER, vSource);
       expect(shader).toBe(null);
     });
 
   test('load shader program successful', () => {
-      const gl = createContext(64, 64);
       const shaderProgram = agentChart.initShaderProgram(gl, agentChart.vSource, agentChart.fSource);
       expect(shaderProgram).not.toBe(null);
     });
 
   test('load shader program unsuccessful', () => {
       const vSource = 'Random garbage';
-      const gl = createContext(64, 64);
       const shaderProgram = agentChart.initShaderProgram(gl, vSource, vSource);
       expect(shaderProgram).toBe(null);
     });
