28095a0e674e330a9f03f38fe936f3c9c5a28d17
Victor van Wieringen <V.J.vanWieringen@student.tudelft.nl>
2020-05-09 14:28:22 +0200

    Add test cases for agentChart

---
+++ app/test/agentChart.test.js
@@ -0,0 +1,30 @@
+import * as agentChart from "../src/scripts/agentChart";
+var gl = require('gl')(64, 64, { preserveDrawingBuffer: true })
+
+// The headless-gl library fails to install on the ewi gitlab CI servers,
+// because some packages are missing. I'm not testing this for now, 
+// but integration testing using selnium would certainly be an option.
+describe('agentChart Test suite', () => {
+  test('load shader successful', () => {
+      const shader = agentChart.loadShader(gl, gl.VERTEX_SHADER, agentChart.vSource);
+      expect(shader).not.toBe(null);
+    });
+
+  test('load shader unsuccessful', () => {
+      const vSource = 'Random garbage'
+      const shader = agentChart.loadShader(gl, gl.VERTEX_SHADER, vSource);
+      expect(shader).toBe(null);
+    });
+
+  test('load shader program successful', () => {
+      const shaderProgram = agentChart.initShaderProgram(gl, agentChart.vSource, agentChart.fSource);
+      expect(shaderProgram).not.toBe(null);
+    });
+
+  test('load shader program unsuccessful', () => {
+      const vSource = 'Random garbage';
+      const shaderProgram = agentChart.initShaderProgram(gl, vSource, vSource);
+      expect(shaderProgram).toBe(null);
+    });
+});
+  
