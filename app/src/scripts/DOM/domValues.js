417df67aca5086a78990559d772f899c27520952
bkolar <b.t.a.kolar@student.tudelft.nl>
2020-05-18 15:52:29 -0400

    Shifted UI layout and added half-page navigation menu

--- app/src/scripts/DOM/domValues.js
+++ app/src/scripts/DOM/domValues.js
@@ -6,10 +6,16 @@ export function getInitialNumInfectious() {
   return parseInt(document.getElementById('initInfectiousCount').value, 10);
 }
 
-export function updateTheStatistics(numSusceptible,numNonInfectious,numInfectious,numImmune,numDead){
-  document.getElementById("s1").innerHTML =`Susceptible ${numSusceptible}` ;
-  document.getElementById("s2").innerHTML =`Non infectious ${numNonInfectious}` ;
-  document.getElementById("s3").innerHTML =`Infectious ${numInfectious}` ;
-  document.getElementById("s4").innerHTML =`Immune ${numImmune}` ;
-  document.getElementById("s5").innerHTML =`Dead ${numDead}` ;
+export function updateTheStatistics(
+  numSusceptible,
+  numNonInfectious,
+  numInfectious,
+  numImmune,
+  numDead
+) {
+  document.getElementById('s1').innerHTML = `${numSusceptible}`;
+  document.getElementById('s2').innerHTML = `${numNonInfectious}`;
+  document.getElementById('s3').innerHTML = `${numInfectious}`;
+  document.getElementById('s4').innerHTML = `${numImmune}`;
+  document.getElementById('s5').innerHTML = `${numDead}`;
 }
