09fedb510059a8b73e3ad211eafa9762385b63db
Hans Sluijter <H.M.Sluijter@student.tudelft.nl>
2020-05-28 15:19:33 +0200

    Add comments to remaining files in data

--- app/src/scripts/data/stats.js
+++ app/src/scripts/data/stats.js
@@ -1,3 +1,4 @@
+/** @class Stats describing the stats of a community or model. */
 export default class Stats {
   constructor(susceptible, noninfectious, infectious, dead, immune) {
     this.susceptible = susceptible;
@@ -7,6 +8,11 @@ export default class Stats {
     this.immune = immune;
   }
 
+  /**
+   * A function summing all of the stats, giving the total number of people represented.
+   * 
+   * @returns {number} The total number of people.
+   */
   sum() {
     return (
       this.susceptible +
@@ -17,6 +23,13 @@ export default class Stats {
     );
   }
 
+  /**
+   * A function allowing us to add together two stats objects.
+   * 
+   * @param {Stats} one An instance of a Stats object to be added.
+   * @param {Stats} other An instance of a Stats object to be added.
+   * @returns {Stats} An instance of a Stats objects representing both parameters added together.
+   */
   static joinStats(one, other) {
     return new Stats(
       one.susceptible + other.susceptible,
