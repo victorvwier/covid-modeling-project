82bad3fda22108a36fc28960d76d9cdbae181c6a
bkolar <b.t.a.kolar@student.tudelft.nl>
2020-06-02 19:14:17 -0400

    Clean UI and stats.js tests

---
+++ app/test/stats.test.js
@@ -0,0 +1,17 @@
+import Stats from '../src/scripts/data/stats';
+
+describe('Stats tests', () => {
+  const stats = new Stats(1, 1, 1, 1, 1);
+  const statsOther = new Stats(1, 1, 1, 1, 1);
+  const statsJoin = new Stats(2, 2, 2, 2, 2);
+
+  test('Test stats sum method', () => {
+    const sum = 5;
+
+    expect(stats.sum()).toBe(sum);
+  });
+
+  test('Test joinStats method', () => {
+    expect(Stats.joinStats(stats, statsOther)).toEqual(statsJoin);
+  });
+});
