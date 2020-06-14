88a931a229b2adca3b800ab36f1821c9dc0828a2
Ji Darwish <j.darwish@student.tudelft.nl>
2020-06-14 12:04:22 +0200

    Pdf version 2 working

--- app/src/scripts/pdfDownloadService.js
+++ app/src/scripts/pdfDownloadService.js
@@ -3,9 +3,26 @@ import JsPdf from 'jspdf';
 export default class PdfDownloadService {
   static createDownloadPdf(data) {
     const html = this._createHTML(data);
+    const sirCanvas = document.getElementById('chart-canvas');
+    const sirChart = sirCanvas.toDataURL('image/jpeg');
+
+    const demographicCanvas = document.getElementById('demographics');
+    const demographicChart = demographicCanvas.toDataURL('image/jpeg');
+
     const doc = new JsPdf();
-    doc.fromHTML(html, 10, 10);
+
+    doc.addImage(sirChart, 'JPEG', 10, 10, 170, 100);
+
+    doc.addImage(demographicChart, 'JPEG', 10, 120, 170, 100);
+
+    doc.fromHTML(html, 10, 300);
     doc.save();
+  }
+
+  static _getDemographicChartImg() {
+    const canvas = document.getElementById('demographics');
+    const img = canvas.toDataURL('image/jpeg');
+    return img;
   }
 
   static _createHTML(data) {
